880fedaf0a6f306e5ac265ff61294950342418ac
jhonatanbds <jhonatanbds@outlook.com>
2016-11-30 17:11:39 -0300

    Quase prontogit add .

--- js/app.js
+++ js/app.js
@@ -1,15 +1,48 @@
 var app = angular.module('toDoList', []);
 app.controller('ToDoController', function($scope) {
+  
   $scope.toDos = [];
-  $scope.addToDo = function() {
-    $scope.toDos.push({name:$scope.newToDo, completed:false});
+  
+  $scope.toDosCompleted = function() {
+	var count = 0;
+	for (i = $scope.toDos.length - 1; i >= 0; i--) {
+		if ($scope.toDos[i].completed) {
+			count++;
+		}
+	}
+	return count;
   };
+  
+  $scope.completedPerCent = function() {
+	  return (100 * ($scope.toDosCompleted()/$scope.toDos.length)).toFixed(1);
+  };
+  
+  $scope.addToDo = function() {
+	if ($scope.newToDo == "")
+		alert("Não");
+	else
+		$scope.toDos.push({name:$scope.newToDo, completed:false});
+  };
+  
   $scope.clear = function() {
     $scope.toDos = [];
   };
-  $scope.clearCompleted = function() {
-    $scope.toDos = _.filter($scope.toDos, function(toDo) {
-      return !toDo.completed;
-    })
+    
+  $scope.clearCompleted = function() {	
+	
+	for (i = $scope.toDos.length - 1; i >= 0; i--) {
+		if ($scope.toDos[i].completed) {
+			$scope.toDos.splice(i, 1);
+		}
+	}
   };
+  
+  $scope.removeToDo = function(index) {
+	
+	$scope.toDos.splice(index, 1);
+	
+  };
+  
+  
+  
 });
